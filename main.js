e2b90444e22d6c321bc0261afcc9ec1f131d3873
brianphan94 <brianphan317@gmail.com>
2021-03-23 19:47:17 -0400

    nav bar working

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-const baseURL = 'https://brianphan94.github.io/'
+const mobileBtn = document.getElementById('mobile-cta')
+const nav = document.querySelector('nav');
+const mobileBtnExit = document.getElementById('mobile-exit');
+        
+    mobileBtn.addEventListener('click' , () => {
+            nav.classList.add('menu-btn');
+        })
+        
+    mobileBtnExit.addEventListener('click' , () => {
+            nav.classList.remove('menu-btn');
+        })
